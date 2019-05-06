19ea7306a44a366d5fe47f57fc2210565e3bab20
Chiamaka Ibeme <chiamakaibeme@chiamakas-mbp.home>
2019-05-07 01:35:35 +1000

    Configuring

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const App = () => {
+    return <header>
+        <h1>Inventory App</h1>
+    </header>
+}
+
+
+
+ReactDOM.render(<App />, document.querySelector('#root'));
+
+
